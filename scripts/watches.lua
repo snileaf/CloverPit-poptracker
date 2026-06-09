d3deb6e09873e7e306546a413e3298a45117fb7c
snileaf <106251014+snileaf@users.noreply.github.com>
2026-06-08 20:11:36 -0400

    Add files via upload

---
+++ scripts/watches.lua
@@ -0,0 +1,7 @@
+ Archipelago:AddClearHandler("clear handler", onClear)
+--Archipelago:AddClearHandler("clear handler", OnClearHandler)
+Archipelago:AddItemHandler("item handler", OnItem)
+Archipelago:AddLocationHandler("location handler", OnLocation)
+
+Archipelago:AddSetReplyHandler("notify handler", OnNotify)
+Archipelago:AddRetrievedHandler("notify launch handler", OnNotifyLaunch)
