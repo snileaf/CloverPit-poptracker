66495052e2f30e872ad6f5b2d880dfabb166a56a
snileaf <106251014+snileaf@users.noreply.github.com>
2026-08-12 10:17:19 -0400

    Delete scripts directory

--- scripts/watches.lua
+++
@@ -1,7 +0,0 @@
-  Archipelago:AddClearHandler("clear handler", OnClear)
---Archipelago:AddClearHandler("clear handler", OnClearHandler)
-Archipelago:AddItemHandler("item handler", OnItem)
-Archipelago:AddLocationHandler("location handler", OnLocation)
-
-Archipelago:AddSetReplyHandler("notify handler", OnNotify)
-Archipelago:AddRetrievedHandler("notify launch handler", OnNotifyLaunch)
